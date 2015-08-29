16dc14d122ffd95cb67f42ae4c0853368c22f4c9
Lahiru <lahirudhananjaya23@gmail.com>
2015-08-29 10:43:57 +0530

    slot updated

--- public/slots/slots.controller.js
+++ public/slots/slots.controller.js
@@ -1,11 +1,23 @@
-(function(){
-	'use strict';
+(function () {
+    'use strict';
 
-	angular
-		.module('iot_app')
-		.controller('SlotsController', slotsController);
+    // var Slot = require('../../api/models/slotModel.js');
 
-	function slotsController ($scope, $http) {
+    angular
+        .module('iot_app')
+        .controller('SlotsController', slotsController);
 
-	};
+    function slotsController($scope, $http) {
+
+        // Function to get the data
+        $scope.getData = function () {
+            $http.get("http://localhost:88/api/get")
+                .success(function (response) {
+                    $scope.names = response;
+                });
+        };
+
+        // Run function every second
+        setInterval($scope.getData, 5000);
+    };
 })();
